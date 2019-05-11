d41166cb389c66b1fa578e969fc84d02d0394858
fumingyang <robotyang123@163.com>
2019-05-11 19:14:20 +0800

    add some sort demo

--- src/Advance/Algorithm/Sort.php
+++ src/Advance/Algorithm/Sort.php
@@ -12,20 +12,169 @@ class Sort
 {
     /**
      * Classical selection sort
+     * - b区 中最小的 val 排到 a区 最后
      * @param $arr
      * @return array
      */
     public static function selectionClassic($arr): array
     {
-        for ($i = 0; $i < count($arr); $i++) {
-            $minInd = $i;
-            for ($j = $i + 1; $j < count($arr); $j++) {
-                if ($arr[$j] < $arr[$minInd]) {
-                    $minInd = $j;
+        for ($i = 0; $i < count($arr); $i++) { //a区
+            $minI = $i;
+            for ($j = $i + 1; $j < count($arr); $j++) { //b区
+                if ($arr[$j] < $arr[$minI]) {
+                    $minI = $j;
                 }
             }
-            list($arr[$i], $arr[$minInd]) = [$arr[$minInd], $arr[$i]];
+            list($arr[$i], $arr[$minI]) = [$arr[$minI], $arr[$i]]; //找到则跨段交换
         }
+
+        return $arr;
+    }
+
+    /**
+     * Classical insertion sort
+     * - 将 b区 中的 val 依次插到 a区中 最合适的位置
+     * @param $arr
+     * @return array
+     */
+    public static function insertionClassic($arr): array
+    {
+        for ($j = 1; $j < count($arr); $j++) { //b区
+            $cmp = $arr[$j];
+            for ($i = $j - 1; $i >= 0; $i--) { //a区
+                if ($cmp < $arr[$i]) { //不是最佳位置
+                    list($arr[$i], $arr[$i + 1]) = [$cmp, $arr[$i]]; //找到则前移交换
+                } else { //找到最佳位置
+                    break;
+                }
+            }
+        }
+
+        return $arr;
+    }
+
+    /**
+     * Classical count sort
+     * - 将 b区 中的 val 依次插到 a区中 最合适的位置
+     * - 计数排序的核心 在于将输入的数据值 转化为 键存储在 额外开辟的 数组空间中。
+     * - 作为一种线性时间复杂度的排序，计数排序要求输入的数据 必须是有确定范围的整数。
+     * @param $arr
+     * @return array
+     */
+    public static function countClassic($arr): array
+    {
+        /*额外开辟的 存储计数的 数组空间*/
+        $crr = array_fill(min($arr), max($arr), 0);
+
+        /*依次计数*/
+        foreach ($arr as $a) {
+            $crr[$a]++;
+        }
+
+        /*依次取出*/
+        $rrr = [];
+        foreach ($crr as $c => $cnt) {
+            while ($cnt > 0) {
+                $rrr[] = $c;
+                $cnt--;
+            }
+        }
+
+        return $rrr;
+    }
+
+    /**
+     * Classical fast sort
+     * - 先从数列中 取出一个数 作为基准数 val
+     * - 分区过程，将比这个数大的数 全放到它的右边，小于或等于它的数 全放到它的左边 (右找小, 左找大)
+     * - 再对左右区间 重复第二步，直到各区间 只有一个数
+     * @param $arr
+     * @return array
+     */
+    public static function fastClassic($arr): array
+    {
+        /*进行循环比较, 最终结果是 val 左边都比它小，右边的都比它大*/
+        $p = 0; //基准数 val 的位置
+        $i = 0;
+        $j = count($arr) - 1;
+        $loopLeft = false;
+        while ($i != $j) {
+            if ($loopLeft) {
+                if ($arr[$i] > $arr[$p]) {
+                    list($arr[$p], $arr[$i]) = [$arr[$i], $arr[$p]];
+                    $p = $i;
+                    $loopLeft = false;
+                } else {
+                    $i++;
+                }
+            } else {
+                if ($arr[$j] < $arr[$p]) {
+                    list($arr[$p], $arr[$j]) = [$arr[$j], $arr[$p]];
+                    $p = $j;
+                    $loopLeft = true;
+                } else {
+                    $j--;
+                }
+            }
+        }
+
+        /*再对 val 左右区间 分别进行 快速排序*/
+        $leftArr = array_slice($arr, 0, $p);
+        $rightArr = array_slice($arr, $p + 1);
+        if ($leftArr) {
+            $leftSort = self::fastClassic($leftArr);
+        }
+        if ($rightArr) {
+            $rightSort = self::fastClassic($rightArr);
+        }
+        $rrr = array_merge($leftSort ?? [], [$arr[$p]], $rightSort ?? []);
+
+        return $rrr;
+    }
+
+    /**
+     * Classical fast sort
+     * - 设待排序元素序列有 n 个元素，首先取一个整数 increment（小于n）作为间隔 将全部元素分为 increment 个子序列
+     * - 所有距离为 increment 的元素放在 同一个子序列中，在每一个子序列中分别实行直接插入排序。
+     * - 然后缩小间隔increment，重复上述子序列划分和排序工作。
+     * - 直到最后取increment=1，将所有元素放在同一个子序列中排序为止。
+     * @param $arr
+     * @return array
+     */
+    public static function shellClassic($arr): array
+    {
+        $len = count($arr);
+        $inc = $len;
+        while (true) {
+            $inc = floor($inc / 3) + 1;
+
+            /*分别获取子序列*/
+            for ($i = 0; $i < $inc; $i++) {
+                $steps = floor(($len - $i) / $inc); //根据间隔获取跳数
+
+                /*获取子序列*/
+                for ($si = 0; $si < $steps; $si++) {
+                    $p = $i + $inc * $si;
+                    $sub[] = $arr[$p];
+                }
+
+                /*执行插入排序*/
+                $sub = self::insertionClassic($sub);
+
+                /*排序后的值, 放回子序列所在位置*/
+                for ($si = 0; $si < $steps; $si++) {
+                    $p = $i + $inc * $si;
+                    $arr[$p] = array_shift($sub);
+                }
+
+                echo 1;
+            }
+
+            if ($inc == 1) {
+                break;
+            }
+        }
+
         return $arr;
     }
 }
--- tests/AlgorithmTest.php
+++ tests/AlgorithmTest.php
@@ -19,12 +19,98 @@ class AlgorithmTest extends TestCase
      */
     public function testSelectionClassic()
     {
-        $randArr = Random::nums(10, 20, 50);
-        echo "rand arr: " . implode(', ', $randArr) . "\n";
+        $rand = Random::nums(10, 20, 50);
 
-        $selArr = Sort::selectionClassic($randArr);
-        echo "sel arr: " . implode(', ', $selArr) . "\n";
+        $t1 = microtime(true);
+        $sort = Sort::selectionClassic($rand);
+        $t2 = microtime(true);
 
+        $data['time'] = round(($t2 - $t1) * 1000, 4) . 'ms';
+        $data['mery'] = round(memory_get_usage() / 1024, 4) . 'kb';
+        $data['rand'] = implode(',', $rand);
+        $data['sort'] = implode(',', $sort);
+
+        print_r($data);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test Classical insertion sort
+     */
+    public function testInsertionClassic()
+    {
+        $rand = Random::nums(10, 20, 50);
+
+        $t1 = microtime(true);
+        $sort = Sort::insertionClassic($rand);
+        $t2 = microtime(true);
+
+        $data['time'] = round(($t2 - $t1) * 1000, 4) . 'ms';
+        $data['mery'] = round(memory_get_usage() / 1024, 4) . 'kb';
+        $data['rand'] = implode(',', $rand);
+        $data['sort'] = implode(',', $sort);
+
+        print_r($data);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test Classical insertion sort
+     */
+    public function testCountClassic()
+    {
+        $rand = Random::nums(10, 20, 50);
+
+        $t1 = microtime(true);
+        $sort = Sort::countClassic($rand);
+        $t2 = microtime(true);
+
+        $data['time'] = round(($t2 - $t1) * 1000, 4) . 'ms';
+        $data['mery'] = round(memory_get_usage() / 1024, 4) . 'kb';
+        $data['rand'] = implode(',', $rand);
+        $data['sort'] = implode(',', $sort);
+
+        print_r($data);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test Classical fast sort
+     */
+    public function testFastClassic()
+    {
+        $rand = Random::nums(10, 20, 50);
+
+        $t1 = microtime(true);
+        $sort = Sort::fastClassic($rand);
+        $t2 = microtime(true);
+
+        $data['time'] = round(($t2 - $t1) * 1000, 4) . 'ms';
+        $data['mery'] = round(memory_get_usage() / 1024, 4) . 'kb';
+        $data['rand'] = implode(',', $rand);
+        $data['sort'] = implode(',', $sort);
+
+        print_r($data);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test Classical shell sort
+     */
+    public function testShellClassic()
+    {
+        $rand = Random::nums(10, 20, 50);
+
+        $t1 = microtime(true);
+        $sort = Sort::shellClassic($rand);
+        $t2 = microtime(true);
+
+        $data['time'] = round(($t2 - $t1) * 1000, 4) . 'ms';
+        $data['mery'] = round(memory_get_usage() / 1024, 4) . 'kb';
+        $data['rand'] = implode(',', $rand);
+        $data['sort'] = implode(',', $sort);
+
+        print_r($data);
         $this->assertTrue(true);
     }
 }
